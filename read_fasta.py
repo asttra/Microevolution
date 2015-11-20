2ae465628f77c5e430f2a25f805906b65f9e6c9c
asttra <nkthomas@gmail.com>
2015-11-19 19:18:55 -1000

    read fasta draft

--- read_fasta.py
+++ read_fasta.py
@@ -4,12 +4,12 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("fasta")
+parser.add_argument("bases")
 args = parser.parse_args()
 
 with open(sys.argv[1], 'r') as gfp_fasta:
     for line in gfp_fasta:
         if line.startswith(">"):
             continue
-        for fasta in line.strip():
-            print ("Fasta File %d:" %  fasta)
+        for bases in line.strip():
+            print bases
