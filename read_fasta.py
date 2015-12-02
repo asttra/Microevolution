82831f706c93b9f7b7684ff29dbb6473636ca725
Niki <nkthomas@hawaii.edu>
2015-12-01 21:03:01 -1000

    with mutate functions

--- read_fasta.py
+++ read_fasta.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python
 
 import sys
+import random
 import argparse
+
+#mutation functions
+def mutate_base(base):
+    if base == 'A':
+        new_base = random.choice('CGT')
+    elif base == 'C':
+        new base = random.choice('AGT')
+    elif base == 'G':
+        new_base == random.choice('ACT')
+    else:
+        new_base == random.choice('ACG')
+    return new_base
+
+def mutate(seq):
+    base_list = list(seq)
+    position_to_mutate = random.randint(0, len(base_list)-1)
+    old_base = base_list[position_to_mutate]
+    new_base = mutate_base(old_base)
+    base_list[position_to_mutate] = new_base
+    new_sequence = "".join(base_list)
+    return new_seq
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("bases")
